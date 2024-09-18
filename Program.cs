c35cd24376b284601ab956736484792bf9191971
temka5228 <artemlazarev2015@gmail.com>
2024-09-18 17:00:03 +0500

    add some sql connection

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
 ﻿using System.Xml.Linq;
-class Program{
-    static void Main(){
-        XDocument xml = XDocument.Load("data.xml");
-        XElement? orders = xml.Element("orders");
+using Npgsql;
 
+class XmlOrder{
+
+}
+
+class Program{
+
+    static async Task Main(string[] args){
+        XDocument xml = XDocument.Load("data.xml");
         var orders_linq = xml.Element("orders")?
             .Elements("order")
             .Select(p => new {
@@ -15,38 +20,24 @@ class Program{
                         quantity = n.Element("quantity")?.Value,
                         name = n.Element("name")?.Value,
                         price = n.Element("price")?.Value
-                    })
+                    }),
+                user =new {
+                    fio = p.Element("user")?.Element("fio")?.Value,
+                    email = p.Element("user")?.Element("email")?.Value}
             });
-
-        if (orders_linq is not null){
+        string connectionString = "Host=localhost;Port=5432;Database=istore;User Id=postgres;Password=1488;";
+        using (NpgsqlConnection connection = new NpgsqlConnection(connectionString)){
+            await connection.OpenAsync();
             foreach(var order in orders_linq){
-                Console.WriteLine($"no = {order.no}\nregdate = {order.reg_date}\nsum = {order.sum}\nproducts = {order.products}");
+                string updateUsers = $" = insert into users (fio, email) select {order.user.fio}, {order.user.email}"+
+                    $" where not exists (select 1 from users where fio = {order.user.fio} and email = {order.user.email});";
+                    Console.WriteLine(updateUsers);
+                NpgsqlCommand command1 = new NpgsqlCommand(updateUsers, connection);
             }
-        }
-
-        if (orders is not null){
-            foreach(XElement order in orders.Elements("order")){
-                XElement? sum = order.Element("sum");
-                Console.WriteLine(sum?.Value);
-                XElement? user = order.Element("user");
-                XElement? fio = user?.Element("fio");
-                XElement? email = user?.Element("email");
-                
-                foreach(XElement product in order.Elements("product")){
-
-
-                    UpdateUsers(user);
-
-                }
-
-            }
+            NpgsqlCommand command = new NpgsqlCommand("select * from users;", connection);
         }
     }
 
-    static void UpdateUsers(XElement? user){
-        XElement? fio = user?.Element("fio");
-        XElement? email = user?.Element("fio");
-    }
 
     static void UpdateProducts(XElement? product){
         XElement? quantity = product?.Element("quantity");
